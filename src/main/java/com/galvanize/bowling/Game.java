e45857199613595114e05267e287345b3e2fd70f
Marilene Soares da Costa <lene626@gmail.com>
2022-07-15 16:36:50 -0600

    cleaning

--- src/main/java/com/galvanize/bowling/Game.java
+++ src/main/java/com/galvanize/bowling/Game.java
@@ -5,18 +5,22 @@ public class Game {
     int rollNumber = 1;
     int score = 0;
     int frameScore = 0;
-    //    boolean spare = false;
+    // bonusCounter keeps track of spares (or strikes) and of how many rolls get the 'bonus' applied (pins * 2)
+    // max bonusCounter = 2
     int bonusCounter = 0;
+    // "strikes" keeps track of strike and of how many rolls get the 'bonus' applied (pins * 2 Or pins * 3)
+    // max strikes = 2
     int strikes = 0;
 
     public int getScore() {
-
         return score;
     }
 
     public int newRoll(int pins) {
-        System.out.println("frame: " + frame);
-        System.out.println("roll Number: " + rollNumber);
+//        System.out.println("frame: " + frame);
+//        System.out.println("roll Number: " + rollNumber);
+
+        // special case: last frame
         if (frame == 10 && pins == 10) {
             score += pins;
             if (rollNumber < 3) {
@@ -26,29 +30,28 @@ public class Game {
         } else {
             // on first roll
             if (rollNumber == 1) {
-                if (bonusCounter > 0) {
+                if (bonusCounter > 0) { // there was a spare; we used 1 or 0 bonus
                     this.frameScore = 2 * pins;
                     if (pins != 10) {
                         this.bonusCounter--;
                         rollNumber++;
-                    } else { // all strikes
+                    } else { // there was a strike (pins == 10)
                         this.bonusCounter = 2;
                         if (strikes < 2) {
                             strikes++;
                         }
-                        if (strikes == 1 || frame == 10) {
+                        if (strikes == 1) {
                             this.frameScore = 2 * pins;
                         } else if (strikes == 2) {
                             this.frameScore = 3 * pins;
                         } else {
                             this.frameScore = pins;
                         }
-
                         score += frameScore;
                         this.rollNumber = 1;
                         frame++;
                     }
-
+                    // bonusCounter = 0, and we have a new strike
                 } else if (pins == 10) {
                     this.bonusCounter = 2;
                     if (strikes < 2) {
@@ -61,12 +64,11 @@ public class Game {
                     } else {
                         this.frameScore = pins;
                     }
-
                     score += frameScore;
                     this.rollNumber = 1;
                     frame++;
 
-                } else {
+                } else { // simple case, no strike
                     this.frameScore = pins;
                     this.rollNumber++;
                 }
@@ -83,18 +85,18 @@ public class Game {
                 rollNumber = 1;
                 frame++;
                 score += frameScore;
-                if (frameScore == 10) {
+                if (frameScore == 10) { // got a spare
                     bonusCounter++;
                 }
             }
         }
 
-        System.out.println("score: " + score);
-        System.out.println("frameScore: " + frameScore);
-
-        System.out.println("bonus Counter: " + bonusCounter);
-        System.out.println("strikes: " + strikes);
-        System.out.println("-------------------");
+//        System.out.println("score: " + score);
+//        System.out.println("frameScore: " + frameScore);
+//
+//        System.out.println("bonus Counter: " + bonusCounter);
+//        System.out.println("strikes: " + strikes);
+//        System.out.println("-------------------");
         return 5;
     }
 }
